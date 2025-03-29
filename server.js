14c7e79e9c4cda76e066ce0ccadc521d66d960f2
federari <ferrarigraziano81@gmail.com>
2025-03-29 11:25:27 +0100

    x

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-const http = require('http');
+const express = require('express');
 require('dotenv').config();
 const admin = require('firebase-admin');
-const path = require('path');
-const multer = require('multer'); // Per gestire l'upload di file
+const multer = require('multer');
 const { Storage } = require('@google-cloud/storage');
 
 // Inizializza l'app di Firebase
@@ -15,216 +14,171 @@ admin.initializeApp({
 
 const db = admin.firestore();
 const bucket = admin.storage().bucket();
+const app = express();
 const port = process.env.PORT || 3000;
 
-
+// Middleware per il parsing del corpo delle richieste
+app.use(express.json()); // per il parsing del corpo JSON
+app.use(express.urlencoded({ extended: true })); // per il parsing del corpo di form data
 
 // Configura multer per gestire l'upload
 const upload = multer({
-    storage: multer.memoryStorage(),
-    limits: { fileSize: 10 * 1024 * 1024 } // Limite di 10MB per le immagini
-  });
+  storage: multer.memoryStorage(),
+  limits: { fileSize: 10 * 1024 * 1024 } // Limite di 10MB per le immagini
+});
 
 // Configura CORS
 const setCorsHeaders = (res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
 };
 
-
+// Funzione per caricare le immagini su Firebase Storage
 const uploadImageToFirebase = (file) => {
-    return new Promise((resolve, reject) => {
-        if (!file) {
-            reject('No file uploaded');
-        }
-        const blob = bucket.file(`images/${Date.now()}_${file.originalname}`);
-        const blobStream = blob.createWriteStream({
-            resumable: false,
-        });
-
-        blobStream.on('error', (err) => {
-            reject(err);
-        });
-
-        blobStream.on('finish', async () => {
-            // Ottieni l'URL pubblico
-            const publicUrl = `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${encodeURIComponent(blob.name)}?alt=media`;
-            resolve(publicUrl);
-        });
-
-        blobStream.end(file.buffer);
+  return new Promise((resolve, reject) => {
+    if (!file) {
+      reject('No file uploaded');
+    }
+    const blob = bucket.file(`images/${Date.now()}_${file.originalname}`);
+    const blobStream = blob.createWriteStream({
+      resumable: false,
     });
+
+    blobStream.on('error', (err) => {
+      reject(err);
+    });
+
+    blobStream.on('finish', async () => {
+      const publicUrl = `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${encodeURIComponent(blob.name)}?alt=media`;
+      resolve(publicUrl);
+    });
+
+    blobStream.end(file.buffer);
+  });
 };
 
-const requestHandler = async (req, res) => {
-    if (req.method === 'OPTIONS') {
-        setCorsHeaders(res);
-        res.writeHead(204);
-        res.end();
-        return;
+// Rotte per il server
+app.options('*', (req, res) => {
+  setCorsHeaders(res);
+  res.sendStatus(204);
+});
+
+app.get('/data', async (req, res) => {
+  setCorsHeaders(res);
+  try {
+    const articlesRef = db.collection('articles');
+    const snapshot = await articlesRef.orderBy('giornata', 'asc').get();
+    if (snapshot.empty) {
+      return res.status(404).json({ error: 'No articles found' });
     }
 
-    if (req.url === '/data' && req.method === 'GET') {
-        setCorsHeaders(res);
-        try {
-            const articlesRef = db.collection('articles');
-            const snapshot = await articlesRef.orderBy('giornata', 'asc').get();
-            if (snapshot.empty) {
-                res.writeHead(404, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify({ error: 'No articles found' }));
-                return;
-            }
+    let articles = [];
+    snapshot.forEach(doc => {
+      articles.push(doc.data());
+    });
 
-            let articles = [];
-            snapshot.forEach(doc => {
-                articles.push(doc.data());
-            });
+    res.status(200).json(articles);
+  } catch (error) {
+    console.error('Error getting documents: ', error);
+    res.status(500).json({ error: 'Error getting articles' });
+  }
+});
 
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify(articles));
-        } catch (error) {
-            console.error('Error getting documents: ', error);
-            res.writeHead(500, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({ error: 'Error getting articles' }));
-        }
-    } else if (req.url === '/upload' && req.method === 'POST') {
-        setCorsHeaders(res);
-        let body = '';
-        req.on('data', chunk => {
-            body += chunk.toString();
-        });
-        console.log(body);
-        req.on('end', async () => {
-            try {
-                const data = JSON.parse(body);
-                const correctPassword = process.env.PASSWORD;  // Questa dovrebbe essere gestita in modo più sicuro
+app.post('/upload', async (req, res) => {
+  setCorsHeaders(res);
+  const { password, ...data } = req.body;
+  const correctPassword = process.env.PASSWORD;
 
-                if (data.password !== correctPassword) {
-                    res.writeHead(401, { 'Content-Type': 'application/json' });
-                    res.end(JSON.stringify({ error: 'Password errata' }));
-                    return;
-                }
+  if (password !== correctPassword) {
+    return res.status(401).json({ error: 'Password errata' });
+  }
 
-                delete data.password;  // Rimuovi la password dai dati salvati
+  try {
+    await db.collection('articles').add(data);
+    res.status(200).json({ success: true });
+  } catch (error) {
+    console.error('Error saving document: ', error);
+    res.status(500).json({ error: 'Error saving article' });
+  }
+});
 
-                await db.collection('articles').add(data);
-                res.writeHead(200, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify({ success: true }));
-            } catch (error) {
-                console.error('Error saving document: ', error);
-                res.writeHead(500, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify({ error: 'Error saving article' }));
-            }
-        });
-    } else if (req.url === '/search' && req.method === 'POST') {
-        setCorsHeaders(res);
-        let body = '';
-        req.on('data', chunk => {
-            body += chunk.toString();
-        });
-        req.on('end', async () => {
-            try {
-                const data = JSON.parse(body);
-                const query = data.query.toLowerCase();
-                console.log(`Received search query: ${query}`); // Log query
-                const articlesRef = db.collection('articles');
-                const snapshot = await articlesRef.get();
+app.post('/search', async (req, res) => {
+  setCorsHeaders(res);
+  const { query } = req.body;
+  const lowerCaseQuery = query.toLowerCase();
 
-                let results = [];
-                snapshot.forEach(doc => {
-                    const article = doc.data();
-                    const titolo = article.titolo || '';
-                    const descrizione = article.descrizione || '';
+  try {
+    const articlesRef = db.collection('articles');
+    const snapshot = await articlesRef.get();
 
-                    if (titolo.toLowerCase().includes(query) || descrizione.toLowerCase().includes(query)) {
-                        results.push(article);
-                    }
-                });
+    let results = [];
+    snapshot.forEach(doc => {
+      const article = doc.data();
+      const titolo = article.titolo || '';
+      const descrizione = article.descrizione || '';
 
-                res.writeHead(200, { 'Content-Type': 'application/json' });
-                console.log(`Search results: ${JSON.stringify(results)}`); // Log results
-                res.end(JSON.stringify(results));
-            } catch (error) {
-                console.error('Error searching documents: ', error);
-                res.writeHead(500, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify({ error: 'Error searching articles' }));
-            }
-        });
-    } else if (req.url === '/prenota' && req.method === 'POST') {
-        setCorsHeaders(res);
-        let body = '';
-        req.on('data', chunk => {
-            body += chunk.toString();
-        });
-        console.log(body);
-        req.on('end', async () => {
-            try {
-                const data = JSON.parse(body);
+      if (titolo.toLowerCase().includes(lowerCaseQuery) || descrizione.toLowerCase().includes(lowerCaseQuery)) {
+        results.push(article);
+      }
+    });
 
-                await db.collection('prenotazioni').add(data);
-                res.writeHead(200, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify({ success: true }));
-            } catch (error) {
-                console.error('Error saving document: ', error);
-                res.writeHead(500, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify({ error: 'Error saving prenotation' }));
-            }
-        });
-    }else if (req.url === '/prenotazioni' && req.method === 'GET') {
-        setCorsHeaders(res);
-        try {
-            const prenotationsRef = db.collection('prenotazioni');
-            const snapshot2 = await prenotationsRef.get();
-            if (snapshot2.empty) {
-                res.writeHead(404, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify({ error: 'No Prenotations Found' }));
-                return;
-            }
+    res.status(200).json(results);
+  } catch (error) {
+    console.error('Error searching documents: ', error);
+    res.status(500).json({ error: 'Error searching articles' });
+  }
+});
 
-            let prenotations = [];
-            snapshot2.forEach(doc => {
-                prenotations.push(doc.data());
-            });
+app.post('/prenota', async (req, res) => {
+  setCorsHeaders(res);
+  try {
+    await db.collection('prenotazioni').add(req.body);
+    res.status(200).json({ success: true });
+  } catch (error) {
+    console.error('Error saving document: ', error);
+    res.status(500).json({ error: 'Error saving prenotation' });
+  }
+});
 
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify(prenotations));
-        } catch (error) {
-            console.error('Error getting documents: ', error);
-            res.writeHead(500, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({ error: 'Error getting prenotations' }));
-        }
-    } else if (req.url === '/uploadImage' && req.method === 'POST') {
-        setCorsHeaders(res);
-        upload.single('immagine')(req, res, async function(err) {
-            if (err) {
-                console.error('Error during file upload:', err);
-                res.writeHead(500, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify({ error: 'Error uploading image' }));
-                return;
-            }
-    
-            console.log('File received:', req.file); // Aggiungi questo log per vedere se multer riceve il file
-    
-            try {
-                const publicUrl = await uploadImageToFirebase(req.file);
-                res.writeHead(200, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify({ imageUrl: publicUrl }));
-            } catch (error) {
-                console.error('Error uploading image to Firebase:', error);
-                res.writeHead(500, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify({ error: 'Error saving image' }));
-            }
-        });
-    } else {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('Not Found');
+app.get('/prenotazioni', async (req, res) => {
+  setCorsHeaders(res);
+  try {
+    const prenotationsRef = db.collection('prenotazioni');
+    const snapshot = await prenotationsRef.get();
+    if (snapshot.empty) {
+      return res.status(404).json({ error: 'No Prenotations Found' });
     }
-};
 
-const server = http.createServer(requestHandler);
+    let prenotations = [];
+    snapshot.forEach(doc => {
+      prenotations.push(doc.data());
+    });
 
-server.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+    res.status(200).json(prenotations);
+  } catch (error) {
+    console.error('Error getting documents: ', error);
+    res.status(500).json({ error: 'Error getting prenotations' });
+  }
+});
+
+app.post('/uploadImage', upload.single('immagine'), async (req, res) => {
+  setCorsHeaders(res);
+  if (req.file) {
+    try {
+      const publicUrl = await uploadImageToFirebase(req.file);
+      res.status(200).json({ imageUrl: publicUrl });
+    } catch (error) {
+      console.error('Error uploading image to Firebase:', error);
+      res.status(500).json({ error: 'Error saving image' });
+    }
+  } else {
+    res.status(400).json({ error: 'No image uploaded' });
+  }
+});
+
+// Avvia il server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
